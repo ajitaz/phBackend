6a6907ef7419a7e6de843eb824ca22a34d90ee4b
ajitaz <azalbert.ab@gmail.com>
2021-02-22 22:14:14 +0545

    backend created

---
+++ addUser.php
@@ -0,0 +1,20 @@
+<?php
+    require 'dbconn.php';
+    require 'header.php';
+
+    $dataJson = file_get_contents('php://input');
+    $data = json_decode($dataJson);
+
+
+    $sql = "INSERT INTO User(username, password ,email, flag) VALUES('$data->username', '$data->password', '$data->email', '$data->flag')";
+    
+    if($conn->query($sql)=== TRUE) {
+        http_response_code(200);
+        echo 'Success';
+    } else{
+        http_response_code(500);
+        echo 'error';
+    }
+
+    mysqli_close($conn);
+?>
---
+++ dbconn.php
@@ -0,0 +1,12 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'plant_hugger';
+
+$conn = new mysqli($hostname,$username,$password,$dbname);
+if (!$conn) {
+    die("Connection failed: ". mysqli_connect_error());
+} else{
+    echo 'success';
+}
