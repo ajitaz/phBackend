02a978c9ced76050ab164f572ac5d40d1a3dc22d
ajitaz <azalbert.ab@gmail.com>
2021-02-24 01:23:19 +0545

    verifyUser added

--- dbconn.php
+++ dbconn.php
@@ -7,6 +7,4 @@ $dbname = 'plant_hugger';
 $conn = new mysqli($hostname,$username,$password,$dbname);
 if (!$conn) {
     die("Connection failed: ". mysqli_connect_error());
-} else{
-    echo 'success';
 }
---
+++ verifyUser.php
@@ -0,0 +1,25 @@
+<?php
+    require 'dbconn.php';
+    require 'header.php';
+
+    $dataJson = file_get_contents('php://input');
+    $data = json_decode($dataJson);
+    $username = $data->username;
+    $password = $data->password;
+
+    $sql = "SELECT username, password FROM User";
+    $status = '';
+    if($res = $conn->query($sql)) {
+        while($row = $res->fetch_row()){
+           if($username === $row[0] && $password === $row[1]){
+               $status = 'VALID';
+           } 
+        }
+        $res->free_result();
+        echo json_encode($status);
+    } else {
+        http_response_code(404);
+    }
+    
+    mysqli_close($conn);
+?>
