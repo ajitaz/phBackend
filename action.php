39c894f32fec0a544c7aac9f57e76fe32cbde519
ajitaz <azalbert.ab@gmail.com>
2021-02-24 18:08:39 +0545

    action page added

---
+++ action.php
@@ -0,0 +1,29 @@
+<?php
+     require 'dbconn.php';
+     require 'header.php';
+
+     $dataJson = file_get_contents('php://input');
+
+    if(!empty($dataJson)){
+        $data = json_decode($dataJson);
+
+       switch($data->value){
+
+        case 'edit' :
+            echo  json_encode('why man');
+            break; 
+
+        case 'delete' :
+            $sql = "DELETE FROM User WHERE id = '4'";
+            if($conn->query($sql)){
+                echo 'true';
+            }
+            break;
+            
+            
+       }
+       
+        
+    }
+
+    mysqli_close($conn);
