0faf4ff6a0f3f8aabe8906d23becaef24fb25e5a
ajitaz <azalbert.ab@gmail.com>
2021-03-15 21:51:35 +0545

    Product Delete function

--- action.php
+++ action.php
@@ -12,14 +12,14 @@ if (!empty($dataJson)) {
         case 'edit':
             $sql = "UPDATE User SET username = '$data->username', email = '$data->email', phone = '$data->phone', flag = '$data->flag' WHERE id = $data->id";
             if ($conn->query($sql)) {
-                echo json_encode('Edited');
+                echo json_encode('User Updated');
             }
             break;
 
         case 'delete':
             $sql = "DELETE FROM User WHERE id = '$data->id'";
             if ($conn->query($sql)) {
-                echo json_encode('Deleted');
+                echo json_encode('User Deleted');
             }
             break;
 
@@ -36,6 +36,13 @@ if (!empty($dataJson)) {
                 echo json_encode('Product Updated');
             }
             break;
+
+            case 'deleteProduct':
+                $sql = "DELETE FROM Product WHERE pid = '$data->pid'";
+                if ($conn->query($sql)) {
+                    echo json_encode('Product Deleted');
+                }
+                break;
     }
 }
 
