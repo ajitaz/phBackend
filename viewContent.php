0139bb84563220f59ea7bf569cef8a194014c43e
ajitaz <azalbert.ab@gmail.com>
2021-07-22 15:51:45 +0545

    new arrival to popular products

--- viewContent.php
+++ viewContent.php
@@ -38,8 +38,8 @@ switch ($option) {
         $sql = 'SELECT O.oid, I.iname, P.pname, P.price, O.quantity, username, O.phone_no, N.name, O.delivery_address, O.full_name FROM Product_Order as O INNER JOIN Product as P ON O.pid = P.pid INNER JOIN Nursery as N ON N.nid = P.nid INNER JOIN Image as I ON P.img_id = I.img_id INNER JOIN User as U ON U.id = O.uid WHERE O.nid = (SELECT nid FROM Nursery_Owner WHERE uid =' . $_GET['uid'] . ') ORDER BY O.oid DESC';
         break;
 
-    case 'viewNewArrival':
-        $sql = 'SELECT nAid, pid, iname, cid FROM New_Arrival NATURAL JOIN Product INNER JOIN Image ON Product.img_id = Image.img_id';
+    case 'viewPopularProducts':
+        $sql = 'SELECT nAid, pid, iname, cid FROM Popular_Products NATURAL JOIN Product INNER JOIN Image ON Product.img_id = Image.img_id';
         break;
 
     case 'emailUser':
